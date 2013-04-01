c0869c3dabb36a8a3c51b2d7bdd63f82f107ea31
ganesan-sree <ganesan@easymedmobile.com>
2013-04-01 13:07:40 +0200

    security with custom page

--- spring/src/com/security/SecurityController.java
+++ spring/src/com/security/SecurityController.java
@@ -1,21 +1,46 @@
 package com.security;
  
+import java.security.Principal;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
  
 @Controller
-@RequestMapping("/login")
+
 public class SecurityController {
  
-	@RequestMapping(method = RequestMethod.GET)
-	public String printWelcome(ModelMap model) {
- 
+	@RequestMapping(value="/security",method = RequestMethod.GET)	
+	public String printWelcome(ModelMap model,Principal principal) {
+		String name = principal.getName();
 		model.addAttribute("message", "Spring Security Hello World");
-		
+		model.addAttribute("username", name);
 		System.out.println("OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
-		return "jsp/security/Security.jsp";
+		return "Security";
+ 
+	}
+	
+	@RequestMapping(value="/login", method = RequestMethod.GET)
+	public String login(ModelMap model) {
+ System.out.println("loginnnnnnnnnnnnnnnnnnnnnnnnnnnn");
+		return "login";
+ 
+	}
+	
+	@RequestMapping(value="/loginfailed", method = RequestMethod.GET)
+	public String loginerror(ModelMap model) {
+ 
+		model.addAttribute("error", "true");
+		System.out.println("errrrrrrrrrrrrrrrrrrroooooooooooooooooooooo");
+		return "login";
+ 
+	}
+	
+	@RequestMapping(value="/logout", method = RequestMethod.GET)
+	public String logout(ModelMap model) {
+		System.out.println("logout");
+		return "login";
  
 	}
  
