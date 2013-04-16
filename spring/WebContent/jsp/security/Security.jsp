bb79731b55c6912c882e54b373ffcb48dd5b3f44
ganesan-sree <ganesan@easymedmobile.com>
2013-04-16 18:27:27 +0530

    security

--- spring/WebContent/jsp/security/Security.jsp
+++ spring/WebContent/jsp/security/Security.jsp
@@ -1,4 +1,5 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+ <%@ taglib uri="http://www.springframework.org/security/tags" prefix="sec"%>
 <html>
 <body>
 	<h1>Message : ${message}</h1>	
@@ -12,5 +13,7 @@
 	
 	<a href="<c:url value="/j_spring_security_logout" />" > Logout</a>
 	
+	
+	         <h1>Welcome! <sec:authentication property="principal.username"/></h1>
 </body>
 </html>
--- spring/src/com/security/SecurityController.java
+++ spring/src/com/security/SecurityController.java
@@ -2,6 +2,9 @@ package com.security;
  
 import java.security.Principal;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,8 +16,26 @@ public class SecurityController {
  
 	@RequestMapping(value="/security",method = RequestMethod.GET)	
 	public String printWelcome(ModelMap model,Principal principal) {
-		String name = principal.getName();
+		
+		
+		User user = (User)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+	    String name1 = user.getUsername(); //get logged in username
+		System.out.println(name1);
+	    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+	    String name2 = auth.getName(); //get logged in username
+	    System.out.println(name2);
+		String name3 = principal.getName();
 		model.addAttribute("message", "Spring Security Hello World");
+		model.addAttribute("username", name3);
+		System.out.println("OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
+		return "Security";
+ 
+	}
+	
+	@RequestMapping(value="/admin",method = RequestMethod.GET)	
+	public String adminCall(ModelMap model,Principal principal) {
+		String name = principal.getName();
+		model.addAttribute("message", "Hello welcome Mr admin !!!!!");
 		model.addAttribute("username", name);
 		System.out.println("OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
 		return "Security";
@@ -39,7 +60,7 @@ public class SecurityController {
 	
 	@RequestMapping(value="/logout", method = RequestMethod.GET)
 	public String logout(ModelMap model) {
-		System.out.println("logout");
+		System.out.println("logout !!!!!!!!!!!!!!!!!!!!!!");
 		return "login";
  
 	}
